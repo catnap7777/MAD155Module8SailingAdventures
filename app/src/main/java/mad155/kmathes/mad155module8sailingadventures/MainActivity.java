d8ebffd09d1cf86b0f3fba60db35533a5a26cfbd
kam <kmathes348@students.mchenry.edu>
2019-03-09 18:10:20 -0600

    Initial commit

---
+++ app/src/main/java/mad155/kmathes/mad155module8sailingadventures/MainActivity.java
@@ -0,0 +1,13 @@
+package mad155.kmathes.mad155module8sailingadventures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
